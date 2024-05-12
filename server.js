1e2fc39e2fbc49354f4a7d9c31f161d33ba0d0ae
srtthomas <thomas.srt@gmail.com>
2024-05-12 17:36:49 +0200

    Commit Dockerfile and App

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello FS2024 DevOps Course after change!!! ');
+    res.send('Azure Cloud Deployment');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
